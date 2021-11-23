dbb0f99d9342a852f0f3402b7d1263b4ea9fb1db
desiretillerasRS <93919310+desiretillerasRS@users.noreply.github.com>
2021-11-23 14:20:16 +0100

    reading-writing-jsonfile

--- server/app.js
+++ server/app.js
@@ -4,58 +4,52 @@ const port = 4000
 var cors = require('cors')
 
 
-function readFile () {
+var readFile =  function() {
 
-    const fs = require('fs');
-    fs.readFile('ChatHistory.json',
-      // callback function that is called when reading file is done
-      function(err, data) {       
-          if (err) throw err;
-          // data is a buffer containing file content
-          console.log(data.toString('utf8'));
-          return JSON.parse(data);
-  });
-    
+  const fs = require('fs');
+  if (fs.existsSync('ChatHistory.json')) {
+
+    let messages = JSON.parse(fs.readFileSync('ChatHistory.json'));
+
+    return messages;
+
+  }
 }
-
-const messages = [];
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    console.log(req);
   res.send('Hello World!')
 })
 
 app.get('/messages', (req, res) => {
-    console.log("returning messages");
- 
-    res.send(readFile());
-
+  console.log("returning messages");
+  res.send(readFile());
 
 })
 
 app.get('/message/:id', (req, res) => {
-    const id = req.params['id'];
-    console.log("returning message", id);
+  const id = req.params['id'];
+  console.log("returning message", id);
   res.send(messages[id])
 })
 
 app.post('/messages', (req, res) => {
 
-    const messages1 = readFile();
-    const id = req.params['id'];
-    const message = req.body;
-    message.id = messages1.length;
+  let messages1 = readFile();
 
-  res.send(message)
+  if (!messages1) messages1 = [];
+  const message = req.body;
+  message.id = messages1.length;
+  messages1.push(message);
 
   const fs = require('fs');
-  fs.writeFile('ChatHistory.json', JSON.stringify(message), function (err) {
+  fs.writeFile('ChatHistory.json', JSON.stringify(messages1), function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
+  res.send(message)
 })
 
 app.listen(port, () => {
